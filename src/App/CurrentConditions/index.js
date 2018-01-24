749c7819a0ab29ded1a476368c58201a48abe847
klequis <klequis@gmail.com>
2018-01-24 12:32:47 -0800

    added live data to CurrentConditions

--- src/App/CurrentConditions/index.js
+++ src/App/CurrentConditions/index.js
@@ -1,23 +1,21 @@
 import React from 'react'
 import './style.css'
-import iCloudy from './cloudy.gif'
+
+import { conditions, temp_f, temp_c, relative_humidity, wind_mph, wind_gust_mph, wind_kph, wind_gust_kph, pressure_mb, pressure_in, pressure_trend, feelslike_f, feelslike_c, icon_url } from '../../data/san-francisco'
 
 const CurrentConditions = () => {
   return (
     <div id='CurrentConditions'>
       <h2>Current Conditions</h2>
-      <img src={iCloudy} alt='cloudy' />
+      <img src={icon_url} alt='cloudy' />
       <ul>
-        <li>Overcast</li>
-        <li>6.4 C</li>
-        <li>Duepoint: 6 C</li>
-        <li>Feels like: 6 C</li>
-        <li>Pressure: 1026 mp 0</li>
-        <li>Relative humidity: 98%</li>
-        <li>UV: 0</li>
+        <li>{conditions}</li>
+        <li>{temp_f} F</li>
+        <li>Feels like: {feelslike_f} F</li>
+        <li>Pressure: {pressure_in} in</li>
+        <li>Relative humidity: {relative_humidity}%</li>
         <li>Wind: West at 0 kph</li>
-        <li>Gusts: 0 kph</li>
-        <li>Windchill: 6 C</li>
+        <li>Gusts: {wind_gust_mph} kph</li>
       </ul>
     </div>
   )
